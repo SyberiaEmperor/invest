3f112d3232781627e772cd005f0226abb8bed6f0
SyberiaImperor <Al.B.danilenko@gmail.com>
2021-12-16 20:24:21 +0300

    jwt controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
+
+require 'jwt'
+require_relative '../helpers/application_helper'
+
 class ApplicationController < ActionController::Base
+
+  def not_found
+    render json: {error: 'not_found'}
+  end
+
+  def authorize_request
+    header = request.headers[:Authorization]
+    header = header.split(' ').last if header
+    begin
+      @decoded = ApplicationHelper::JsonWebToken.decode(header)
+      #@current_user = User.find(@decoded[:user_id]) TODO by AppRecord
+    rescue ActiveRecord::RecordNotFound => e
+      render json: {errors: e.message}, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: {errors: e.message}, status: :unauthorized
+    end
+  end
 end
